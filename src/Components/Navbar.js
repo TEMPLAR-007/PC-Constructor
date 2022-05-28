504d4c38e2f4b9a1365a011f95e9da7f3fa4dbde
TEMPLAR-007 <arefin.khan8364@gmail.com>
2022-05-28 19:51:16 +0600

    page done

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -55,8 +55,11 @@ const Navbar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                 </label>
             </div >
+
+            asdasdasdasdads
         </div >
     );
 };
 
 export default Navbar;
+
