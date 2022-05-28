9dd36987532e53fcd6d1d73c33cfb9a61a498d81
TEMPLAR-007 <arefin.khan8364@gmail.com>
2022-05-28 19:39:49 +0600

    Contact done

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -38,10 +38,8 @@ const ContactUs = () => {
             </div >
 
             <div dashboard-sidebar className='map shadow-2xl' >
-                <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116834.00977793321!2d90.34928583919222!3d23.780777744454788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1652015756933!5m2!1sen!2sbd" width="100%" height="480" style="border:0;" allowfullscreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></Iframe>
+                <Iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116834.00977793321!2d90.34928583919222!3d23.780777744454788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka!5e0!3m2!1sen!2sbd!4v1652015756933!5m2!1sen!2sbd" width="100%" height="480" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></Iframe>
             </div >
-
-
         </div >
     );
 };
